9e9bc537b86c320beb0b3fc9cc54b4b0b6dcb3d4
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-11-27 18:14:08 +0330

    feat: create routes by link component

--- app/components/Navigation.js
+++ app/components/Navigation.js
@@ -1,5 +1,17 @@
-import React from "react";
+import Link from "next/link";
 
 export default function Navigation() {
-  return <div>Navigation</div>;
+  return (
+    <ul>
+      <li>
+        <Link href="/cabins"> Cabins </Link>
+      </li>
+      <li>
+        <Link href="/about"> About </Link>
+      </li>
+      <li>
+        <Link href="/account"> Your account </Link>
+      </li>
+    </ul>
+  );
 }
