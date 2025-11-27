369d3d250b8f4404c9efbe4b5eb52899f4f8e70b
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-11-27 18:50:07 +0330

    add sources files for project

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ export default function Page() {
   return (
     <div>
       <h1> the wild oasis elcome to pradias </h1>
-      <Link href="/cabins"> Explore luxury cabins </Link>
+      {/* <Link href="/cabins"> Explore luxury cabins </Link> */}
     </div>
   );
 }
