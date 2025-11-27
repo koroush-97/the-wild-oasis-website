6c6471fb28fd60ce0ee5985da75d2d34cb658d23
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-11-27 18:40:06 +0330

    add navigation layout for all app

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,3 +1,9 @@
+import Navigation from "../components/Navigation";
+
 export default function Page() {
-  return <h1>Cabins page</h1>;
+  return (
+    <div>
+      <h1>Cabins page</h1>
+    </div>
+  );
 }
