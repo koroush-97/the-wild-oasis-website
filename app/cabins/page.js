f52e6c5410622562ed0acaf73433ccd4a05f6c66
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-11-28 21:00:28 +0330

    feat(title) : add metadata for title component

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,15 +1,11 @@
-export default async function Page() {
-  const res = await fetch("https://jsonplaceholder.typicode.com/users");
-  const data = await res.json();
+export const metadata = {
+  title: "Cabins",
+};
 
+export default function Page() {
   return (
     <div>
-      <h1>Cabins page</h1>
-      <ul>
-        {data.map((user) => (
-          <li key={user.id}> {user.name} </li>
-        ))}
-      </ul>
+      <h1>Cabins page </h1>
     </div>
   );
 }
