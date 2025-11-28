6665bd3404aa3d670931ef806fa04adac950f8cf
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-11-28 20:07:44 +0330

    add tailwind css to layout of project

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,7 @@
-import Logo from "./components/Logo";
-import Navigation from "./components/Navigation";
+import Logo from "./_components/Logo";
+import Navigation from "./_components/Navigation";
+
+import "@/app/_styles/globals.css";
 
 export const metadata = {
   title: "The Wild Oasis",
