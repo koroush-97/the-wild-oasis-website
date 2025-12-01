a291573831a679fe2da0ffba65db5c197f2c2b67
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-12-01 18:50:00 +0330

    fix typo of url syntax error

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,31 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: "https",
+//         hostname: "https://vdclfcjrdmwrsxpeihhw.supabase.co",
+//         port: "",
+//         pathname: "/storage/v1/object/public/cabin-images/**",
+//       },
+//     ],
+//   },
+// };
+
+// export default nextConfig;
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "vdclfcjrdmwrsxpeihhw.supabase.co",
+        port: "",
+        pathname: "/storage/v1/object/public/cabin-images/**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
