1c13d497bc356ad372b1e71cb736dd5ff2af4a2e
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-12-03 12:08:22 +0330

    fix typo of getcabin

--- app/_components/CabinList.js
+++ app/_components/CabinList.js
@@ -1,8 +1,10 @@
-import { getCabin } from "../_lib/data-service";
+import { getCabins } from "../_lib/data-service";
 import CabinCard from "./CabinCard";
 
 async function CabinList() {
-  const cabins = await getCabin();
+  const cabins = await getCabins();
+
+  if (!cabins.length) return null;
 
   return (
     <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
