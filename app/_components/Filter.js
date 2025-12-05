e6ec19d861bdfcbeea3f87c2c1b02048a6027913
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-12-05 17:28:36 +0330

    feat(filter): add capacity filter buttons with active state and URL sync
    Includes:
    
    Implement capacity filter using URLSearchParams
    
    Add reusable Button component
    
    Highlight active filter based on search params
    
    Update router state using replace() without scroll

--- app/_components/Filter.js
+++ app/_components/Filter.js
@@ -1,22 +1,65 @@
 "use client";
 
+import { usePathname, useSearchParams, useRouter } from "next/navigation";
+
 function Filter() {
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const activeFilter = searchParams.get("capacity") ?? "all";
+
+  function handleFilter(filter) {
+    const params = new URLSearchParams(searchParams);
+    params.set("capacity", filter);
+    router.replace(`${pathname}?${params.toString()}`, { scroll: false });
+  }
+
   return (
     <div className="border border-primary-800 flex">
-      <button className="px-5 py-2 hover:bg-primary-700"> All cabins </button>
-      <button className="px-5 py-2 hover:bg-primary-700">
-        {" "}
-        1&mdash;3 guests{" "}
-      </button>
-      <button className="px-5 py-2 hover:bg-primary-700">
-        {" "}
-        4&mdash;7 guests{" "}
-      </button>
-      <button className="px-5 py-2 hover:bg-primary-700">
-        {" "}
-        8&mdash;12 guests{" "}
-      </button>
+      <Button
+        filter="all"
+        handleFilter={handleFilter}
+        activeFilter={activeFilter}
+      >
+        All cabins
+      </Button>
+      <Button
+        filter="small"
+        handleFilter={handleFilter}
+        activeFilter={activeFilter}
+      >
+        2&mdash;3 guests
+      </Button>
+      <Button
+        filter="medium"
+        handleFilter={handleFilter}
+        activeFilter={activeFilter}
+      >
+        4&mdash;7 guests
+      </Button>
+      <Button
+        filter="large"
+        handleFilter={handleFilter}
+        activeFilter={activeFilter}
+      >
+        8&mdash;12 guests
+      </Button>
     </div>
+  );
+}
+
+function Button({ filter, handleFilter, activeFilter, children }) {
+  return (
+    <button
+      onClick={() => handleFilter(filter)}
+      className={`px-5 py-2 hover:bg-primary-700 ${
+        filter === activeFilter ? "bg-primary-700 text-primary-50" : ""
+      } `}
+    >
+      {" "}
+      {children}
+    </button>
   );
 }
 
