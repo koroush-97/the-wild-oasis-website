bd37f38ee1babe37d938478df29e3abccd55d106
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-12-09 19:53:55 +0330

    fix(reservation): import auth for session check

--- app/_components/Reservation.js
+++ app/_components/Reservation.js
@@ -1,12 +1,14 @@
 import DateSelector from "@/app/_components/DateSelector";
 import ReservationForm from "./ReservationForm";
 import { getBookedDatesByCabinId, getSettings } from "../_lib/data-service";
+import LoginMessage from "./LoginMessage";
 
 async function Reservation({ cabin }) {
   const [settings, bookedDates] = await Promise.all([
     getSettings(),
     getBookedDatesByCabinId(cabin.id),
   ]);
+  const session = await auth();
 
   return (
     <div className="grid grid-cols-2 border border-primary-800 min-h-[400px]">
@@ -15,7 +17,7 @@ async function Reservation({ cabin }) {
         bookedDates={bookedDates}
         cabin={cabin}
       />
-      <ReservationForm cabin={cabin} />
+      {session?.user ? <ReservationForm cabin={cabin} /> : <LoginMessage />}
     </div>
   );
 }
