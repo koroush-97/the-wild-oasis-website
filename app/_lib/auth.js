db2d72214280c551f9fab3b61f6685fbb35e61c9
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-12-10 11:46:27 +0330

    chore(auth): remove redundant authorized callback

--- app/_lib/auth.js
+++ app/_lib/auth.js
@@ -8,6 +8,11 @@ const authCongfig = {
       clientSecret: process.env.AUTH_GOOGLE_SECRET,
     }),
   ],
+  callbacks: {
+    authorized({ auth, request }) {
+      return !!auth?.user;
+    },
+  },
 };
 
 export const {
