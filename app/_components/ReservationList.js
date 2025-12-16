c46c21eeda186640246eb08bd2df471a8f6d2945
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-12-16 23:10:53 +0330

    feat(reservations): implement optimistic deletion with handleDelete callback

--- app/_components/ReservationList.js
+++ app/_components/ReservationList.js
@@ -2,17 +2,29 @@
 
 import { useOptimistic } from "react";
 import ReservationCard from "./ReservationCard";
+import { deleteReservation } from "../_lib/actions";
 
 export default function ReservationList({ bookings }) {
   const [optimisticBookings, optimisticDelete] = useOptimistic(
     bookings,
-    () => {}
+    (curBookings, bookingId) => {
+      return curBookings.filter((booking) => booking.id !== bookingId);
+    }
   );
+
+  async function handleDelete(bookingId) {
+    optimisticBookings(bookingId);
+    await deleteReservation(bookingId);
+  }
 
   return (
     <ul className="space-y-6">
-      {bookings.map((booking) => (
-        <ReservationCard booking={booking} key={booking.id} />
+      {optimisticBookings.map((booking) => (
+        <ReservationCard
+          booking={booking}
+          onDelete={handleDelete}
+          key={booking.id}
+        />
       ))}
     </ul>
   );
