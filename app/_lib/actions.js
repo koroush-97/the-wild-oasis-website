906f4d0b872b84659e8216c64312d69c90058b6d
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-12-16 17:13:02 +0330

    fix(actions): move booking verification to deleteReservation function

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -3,16 +3,21 @@
 import { signIn, signOut, auth } from "@/app/_lib/auth";
 import { supabase } from "./supabase";
 import { revalidatePath } from "next/cache";
+import { getBookings } from "./data-service";
 
 export async function updateGuest(formData) {
   const session = await auth();
   if (!session) throw new Error("You must be logged in");
 
+  const guestBookings = await getBookings(session.user.guestId);
+  const guestBookingIds = guestBookings.map((booking) => booking.id);
+
+  if (!guestBookingIds.includes(bookingId))
+    throw new Error("You are not allowed to delete this booking");
+
   const nationalID = formData.get("nationalID");
   const [nationality, countryFlag] = formData.get("nationality").split("%");
 
-  // if (!/^[a-zA-Z0-9]{6,12}$/.test(nationalID))
-  //   throw new Error("Please provider a valid national ID");
   if (!nationalID) throw new Error("National ID is required");
 
   if (!/^\d{10}$/.test(nationalID))
