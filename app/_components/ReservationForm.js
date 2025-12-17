db2273d1999da15a316cf4349f52bca7bd67b250
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-12-17 23:47:29 +0330

    feat(reservation): assemble booking payload and prepare createBooking call

--- app/_components/ReservationForm.js
+++ app/_components/ReservationForm.js
@@ -2,17 +2,28 @@
 
 import { differenceInDays } from "date-fns";
 import { useReservation } from "./ReservationContext";
+import { createBooking } from "../_lib/actions";
 
 function ReservationForm({ cabin, user }) {
   // CHANGE
   const { range } = useReservation();
-  const { maxCapacity, regularPrice, discount } = cabin;
+  const { maxCapacity, regularPrice, discount, id } = cabin;
 
   const startDate = range?.from;
   const endDate = range?.to;
 
   const numNights = differenceInDays(endDate, startDate);
   const cabinPrice = numNights * (regularPrice - discount);
+
+  const bookingDate = {
+    startDate,
+    endDate,
+    numNights,
+    cabinPrice,
+    cabinId: id,
+  };
+
+  createBooking.bind(null, bookingDate);
 
   return (
     <div className="scale-[1.01]">
@@ -32,7 +43,10 @@ function ReservationForm({ cabin, user }) {
         </div>
       </div>
 
-      <form className="bg-primary-900 py-10 px-16 text-lg flex gap-5 flex-col">
+      <form
+        action={createBooking}
+        className="bg-primary-900 py-10 px-16 text-lg flex gap-5 flex-col"
+      >
         <div className="space-y-2">
           <label htmlFor="numGuests">How many guests?</label>
           <select
